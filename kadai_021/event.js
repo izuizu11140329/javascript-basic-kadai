754b353963aa3d1958acb9de7f38a92d88e04aa8
izuizu11140329 <amiriakk53@gmail.com>
2025-03-16 15:19:47 +0900

    kadai_021をアップロードする

---
+++ kadai_021/event.js
@@ -0,0 +1,27 @@
+//btnというidを持つHTML要素を取得して、定数に代入する（クリックする場所,bmeiは宣言した定数名）
+const bmei = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する（元から表示されていた場所、textmeiは宣言した定数名）
+const textmei = document.getElementById('text');
+
+
+
+//HTML要素がクリックされたときにイベント処理を実行する
+bmei.addEventListener('click',() => {
+    
+setTimeout(()=> {      // イベント処理の中で非同期処理の記述する！
+
+//li要素を新しく追加する(clildListは新しいli要素の定数名)
+const childList = document.createElement('li');
+//作成したli要素に「ボタンをクリックしました」というテキストを追加する
+childList.textContent = 'ボタンをクリックしました';
+// 作成したli要素をul要素の子要素として末尾に追加する
+textmei.appendChild(childList);
+
+ // id="text"の文章を書き換えるには、textContent を使う。
+ text.textContent    = "ボタンをクリックしました";
+
+   },2000);  //これで2秒後に実行になる！
+
+}); 
+
