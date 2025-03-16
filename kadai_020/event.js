3c98632c96282a42744ab837b0841861a6a06ddc
izuizu11140329 <amiriakk53@gmail.com>
2025-03-16 17:24:48 +0900

    kadai_020再提出する

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -7,16 +7,6 @@ const textmei = document.getElementById('text');
 //HTML要素がクリックされたときにイベント処理を実行する
 bmei.addEventListener('click',() => {
 
-
-// TODO: appendChild の場合、HTML要素を追加することになってしまう。
-
-//li要素を新しく追加する(clildListは新しいli要素の定数名)
-const childList = document.createElement('li');
-//作成したli要素に「ボタンをクリックしました」というテキストを追加する
-childList.textContent = 'ボタンをクリックしました';
-// 作成したli要素をul要素の子要素として末尾に追加する
-textmei.appendChild(childList);
-
  // id="text"の文章を書き換えるには、textContent を使う。
 
  textmei.textContent    = "ボタンをクリックしました";
