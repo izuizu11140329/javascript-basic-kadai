fcee83acb7bf4db9274ffbbf8eb28b82d70b63dc
izuizu11140329 <amiriakk53@gmail.com>
2025-03-16 17:38:22 +0900

    kadai_021提出する

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -11,12 +11,6 @@ bmei.addEventListener('click',() => {
 
 setTimeout(()=> {      // イベント処理の中で非同期処理の記述する！
 
-//li要素を新しく追加する(clildListは新しいli要素の定数名)
-const childList = document.createElement('li');
-//作成したli要素に「ボタンをクリックしました」というテキストを追加する
-childList.textContent = 'ボタンをクリックしました';
-// 作成したli要素をul要素の子要素として末尾に追加する
-textmei.appendChild(childList);
 
  // id="text"の文章を書き換えるには、textContent を使う。
  textmei.textContent    = "ボタンをクリックしました";
